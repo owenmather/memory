4e240d101a910eb99c93111722d978733ed082e3
owenmather <owenmather7@gmail.com>
2016-04-27 18:02:58 +0100

    Setup
    Setup

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name memoryApp
+ * @description
+ * # memoryApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('memoryApp', []);
---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name memoryApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the memoryApp
+ */
+angular.module('memoryApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
